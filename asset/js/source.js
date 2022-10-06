91ef40f54214637bbfd4a00bd317b4622a5f62ca
johnson <johnson.dev298@gmail.com>
2022-10-06 21:55:12 +0700

    s

--- asset/js/source.js
+++ asset/js/source.js
@@ -6,6 +6,8 @@ $sliderMobileOnly.slick({
     slidesToScroll: 1,
     mobileFirst: true,
     dots: false,
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="asset/images/icon/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="asset/images/icon/arrow-right.svg" alt=""></button>',
     responsive: [
         {
             breakpoint: 768,
